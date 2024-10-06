b3e99e8cfb773d4eaf2b8510bcd4c555210692d6
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2024-10-06 10:42:01 +0100

    chore(config): added react-force-graph and vitest dependencies + pillow data

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  environment: 'jsdom',
+  include: ['**/*.spec.{ts,tsx}'],
+  globals: true,
+  mockReset: true,
+  css: {
+    modules: {
+      classNameStrategy: 'non-scoped'
+    }
+  },
+  coverage: {
+    provider: 'v8',
+    reporter: ['text', 'html'],
+    exclude: [
+      'node_modules/'
+    ],
+  },
 })
