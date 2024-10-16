5ef49a37079468911f7942b3429dc6e6bdfb291e
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2024-10-16 10:37:53 +0100

    feat(graph): added language toggle button

--- src/pages/SleepPage.tsx
+++ src/pages/SleepPage.tsx
@@ -1,17 +1,23 @@
 import styles from './SleepPage.module.scss'
 import {MetricConfiguration} from "modules/controls/MetricConfiguration";
-import {Spin} from "antd";
+import {Spin, Switch} from "antd";
 import {LoadingOutlined} from "@ant-design/icons";
 import {DateRangePicker} from "modules/controls/DateRangePicker";
 import {SleepSessionsGraph2D} from "modules/graph/components/SleepSessionsGraph2D";
 import {useSleepContext} from "context";
 import {useTranslation} from "react-i18next";
 import {useGraphStyles} from "modules/graph/hooks/useGraphStyles";
+import {useCallback} from "react";
 
 export const SleepPage = () => {
   const { currentMetricColour } = useGraphStyles()
-  const { t } = useTranslation('translation', { keyPrefix: 'sleep.graph2d' })
+  const { t, i18n } = useTranslation('translation', { keyPrefix: 'sleep.graph2d' })
   const { activeSessions, sleepData, isSleepDataLoading } = useSleepContext()
+
+  const handleChangeLanguage = useCallback(() => {
+    const isEnglish = i18n.language === 'en'
+    i18n.changeLanguage(isEnglish ? 'jp' : 'en')
+  }, [i18n])
 
   if (isSleepDataLoading) {
     return (
@@ -24,13 +30,21 @@ export const SleepPage = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.sessions}>
-        <span style={{ color: currentMetricColour }}>
+      <div className={styles.topLeftControls}>
+        <p style={{ color: currentMetricColour }} className={styles.sessions}>
           {t('sessions', {
             active: activeSessions,
             total: sleepData?.sessions.length
           })}
-        </span>
+        </p>
+
+        <Switch
+          size='small'
+          onChange={handleChangeLanguage}
+          className={styles.languageSwitch}
+          checkedChildren={t('language.checked')}
+          unCheckedChildren={t('language.unchecked')}
+        />
       </div>
 
       <div className={styles.controls}>
