7a78b8dcb188c125b58c4ec735f65ae85b16f077
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2024-10-17 11:25:24 +0100

    test(data): added linear regression hook unit tests and shortened english translations

---
+++ src/data/useLinearRegression/useLinearRegression.spec.ts
@@ -0,0 +1,122 @@
+import {renderHook} from "@testing-library/react";
+import {useLinearRegression} from "data/useLinearRegression/useLinearRegression.ts";
+import {SleepMetric} from "modules/controls/MetricConfiguration";
+import {SleepSessionGraph2DData} from "modules/graph/components/SleepSessionsGraph2D";
+
+const mockSleepContext = vi.fn()
+vi.mock('context/useSleepContext', () => ({
+  useSleepContext: () => mockSleepContext()
+}))
+
+const validGraphData2D: SleepSessionGraph2DData = [
+  {
+    _date: 'Aug 24',
+    date: new Date(2024, 7, 24),
+    duration: 520,
+    [SleepMetric.AWAKE_TIME]: 10,
+    [SleepMetric.DEEP_SLEEP]: 60,
+    [SleepMetric.LIGHT_SLEEP]: 300,
+    [SleepMetric.QUALITY]: 84,
+    [SleepMetric.REM_SLEEP]: 70
+  },
+  {
+    _date: 'Aug 25',
+    date: new Date(2024, 7, 25),
+    duration: 520,
+    [SleepMetric.AWAKE_TIME]: 10,
+    [SleepMetric.DEEP_SLEEP]: 20,
+    [SleepMetric.LIGHT_SLEEP]: 300,
+    [SleepMetric.QUALITY]: 84,
+    [SleepMetric.REM_SLEEP]: 70
+  }
+]
+
+describe('Linear Regression Hook', () => {
+  it('should return a valid array of straight-line coordinates for the regression line data', () => {
+    mockSleepContext.mockReturnValueOnce({
+      sleepMetric: SleepMetric.DEEP_SLEEP,
+      graphData2d: {
+        data: validGraphData2D,
+        isSleepDataLoading: false
+      }
+    })
+
+    const { result } = renderHook(useLinearRegression)
+
+    expect(result.current.regressionLineData).toStrictEqual([
+      {
+        _date: "Aug 24",
+        y: 59.99999932863284,
+      },
+      {
+        _date: "Aug 24",
+        y: 20.000000671483576,
+      },
+    ])
+  })
+
+  it('should return a the y-ordinate that the regression line delta triangles horizontal line intercepts the y-axis', () => {
+    mockSleepContext.mockReturnValueOnce({
+      sleepMetric: SleepMetric.DEEP_SLEEP,
+      graphData2d: {
+        data: validGraphData2D,
+        isSleepDataLoading: false
+      }
+    })
+
+    const { result } = renderHook(useLinearRegression)
+
+    expect(result.current.yRegressionDeltaLine).toBe(59.99999932863284)
+  })
+
+  it('should return a the x-ordinate (as a date index) that the regression line delta triangles vertical line intercepts the x-axis', () => {
+    mockSleepContext.mockReturnValueOnce({
+      sleepMetric: SleepMetric.DEEP_SLEEP,
+      graphData2d: {
+        data: validGraphData2D,
+        isSleepDataLoading: false
+      }
+    })
+
+    const { result } = renderHook(useLinearRegression)
+
+    expect(result.current.xRegressionDeltaLine).toBe(1)
+  })
+
+  it('should return the difference between the highest and lowest sleep metric value as the regression line delta', () => {
+    mockSleepContext.mockReturnValueOnce({
+      sleepMetric: SleepMetric.DEEP_SLEEP,
+      graphData2d: {
+        data: validGraphData2D,
+        isSleepDataLoading: false
+      }
+    })
+
+    const { result } = renderHook(useLinearRegression)
+
+    expect(result.current.regressionDelta).toBeCloseTo(-40.0)
+  })
+
+  it('should return the current sleep metric enum string as the data key', () => {
+    mockSleepContext.mockReturnValue({
+      sleepMetric: SleepMetric.DEEP_SLEEP,
+      graphData2d: {
+        data: [{
+          _date: 'Aug 25',
+          date: new Date(2024, 7, 25),
+          duration: 520,
+          [SleepMetric.AWAKE_TIME]: 10,
+          [SleepMetric.DEEP_SLEEP]: 20,
+          [SleepMetric.LIGHT_SLEEP]: 300,
+          [SleepMetric.QUALITY]: 84,
+          [SleepMetric.REM_SLEEP]: 70
+        }],
+        isSleepDataLoading: false
+      }
+    })
+
+    const { result } = renderHook(useLinearRegression)
+
+    expect(result.current.regressionDataKey).toBe(SleepMetric.DEEP_SLEEP)
+  })
+})
