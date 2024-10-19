21fecc1c4d7336030d8cdf11a886815efc31d77d
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2024-10-19 19:10:25 +0100

    feat(graph): show all button now changes to "recent" if all sessions are showing

--- src/modules/controls/ShowAllButton/ShowAllButton.tsx
+++ src/modules/controls/ShowAllButton/ShowAllButton.tsx
@@ -2,22 +2,39 @@ import { Button } from 'antd'
 import { useTranslation } from 'react-i18next'
 import { useSleepContext } from 'context'
 import { useDateSelection } from 'modules/graph/hooks/useDateSelection'
-import { useCallback } from 'react'
+import { useCallback, useMemo } from 'react'
+import dayjs from 'dayjs'
 
 export const ShowAllButton = () => {
-  const { sleepData } = useSleepContext()
   const { setDateRange } = useDateSelection()
-  const { t } = useTranslation('translation', { keyPrefix: 'sleep.graph-controls' })
+  const { sleepData, graphData2d, rangeStart, rangeEnd } = useSleepContext()
+  const { t } = useTranslation('translation', { keyPrefix: 'sleep.graph-controls.show-all' })
+
+  const isShowingAll = useMemo<boolean>(() => {
+    if (graphData2d && sleepData) {
+      const earliestRecordedSession = sleepData.earliestSession
+      const latestRecordedSession = sleepData.latestSession
+      const isShowingLowerBound = rangeStart.getTime() === earliestRecordedSession.getTime()
+      const isShowingUpperBound = rangeEnd.getTime() === latestRecordedSession.getTime()
+      return isShowingLowerBound && isShowingUpperBound
+    }
+
+    return false
+  }, [graphData2d, rangeEnd, rangeStart, sleepData])
 
   const handleClick = useCallback(() => {
-    if (sleepData) {
-      setDateRange(sleepData.earliestSession, sleepData.latestSession)
+    if (graphData2d && sleepData) {
+      if (isShowingAll) {
+        setDateRange(dayjs(graphData2d.latestSession).subtract(2, 'month').toDate(), graphData2d.latestSession)
+      } else {
+        setDateRange(sleepData.earliestSession, sleepData.latestSession)
+      }
     }
-  }, [setDateRange, sleepData])
+  }, [graphData2d, sleepData, isShowingAll, setDateRange])
 
   return (
     <Button onClick={handleClick} size='small' color='default'>
-      {t('show-all')}
+      {isShowingAll ? t('default') : t('all')}
     </Button>
   )
 }
