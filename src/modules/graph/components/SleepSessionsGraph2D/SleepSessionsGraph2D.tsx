76a2a2e99a678df85fc05414d73216ddc877acc0
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2024-10-19 12:11:57 +0100

    fix(routing): fixed bad query param name for sleep metric

--- src/modules/graph/components/SleepSessionsGraph2D/SleepSessionsGraph2D.tsx
+++ src/modules/graph/components/SleepSessionsGraph2D/SleepSessionsGraph2D.tsx
@@ -28,7 +28,6 @@ export const SleepSessionsGraph2D = () => {
   const { typicalSleepSession , typicalSleepSessionFill } = useTypicalSession()
   const { currentMetricColour, strokeWidth, activeDotRadius } = useGraphStyles()
   const { graphData2d: { data, earliestSession, latestSession }, sleepMetric } = useSleepContext()
-  console.log(yDomain, yTicks)
 
   const {
     regressionLineData,
