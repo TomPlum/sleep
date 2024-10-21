5a2142935f96e5ec67d45958782a6d57c4fdd8ff
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2024-10-21 10:35:42 +0100

    feat(graph): added data source version to active session info

--- src/modules/graph/components/ActiveSessionInfo/ActiveSessionInfo.tsx
+++ src/modules/graph/components/ActiveSessionInfo/ActiveSessionInfo.tsx
@@ -8,23 +8,29 @@ import classNames from 'classnames'
 
 export const ActiveSessionInfo = ({ className }: ActiveSessionInfoProps ) => {
   const { currentMetricColour } = useGraphStyles()
-  const { t } = useTranslation('translation', { keyPrefix: 'sleep.graph2d' })
   const { activeSessions, sleepData } = useSleepContext()
+  const { t } = useTranslation('translation', { keyPrefix: 'sleep.graph2d' })
 
   return (
     <div className={classNames(styles.container, className)}>
-      <a href='https://github.com/TomPlum/sleep' rel='noreferrer'>
-        <GithubOutlined
-          className={styles.github}
-        />
-      </a>
+      <div className={styles.top}>
+        <a href='https://github.com/TomPlum/sleep' rel='noreferrer'>
+          <GithubOutlined
+            className={styles.github}
+          />
+        </a>
 
-      <p style={{ color: currentMetricColour }} className={styles.sessions}>
-        {t('sessions', {
-          active: activeSessions,
-          total: sleepData?.sessions.length,
-          naps: sleepData?.sessions.filter(session => session.isNap).length
-        })}
+        <p style={{ color: currentMetricColour }} className={styles.sessions}>
+          {t('sessions', {
+            active: activeSessions,
+            total: sleepData?.sessions.length,
+            naps: sleepData?.sessions.filter(session => session.isNap).length
+          })}
+        </p>
+      </div>
+
+      <p className={styles.dataExportVersion}>
+        {t('data-source')}
       </p>
     </div>
   )
