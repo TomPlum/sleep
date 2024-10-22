e33383e34c406d805ee9398ca2ce653fc8ccf1a4
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2024-10-22 15:10:35 +0100

    feat(graph): stacked graph placeholder now offer available sleep metrics to pick from

--- src/modules/graph/components/StackedGraphPlaceholder/StackedGraphPlaceholder.tsx
+++ src/modules/graph/components/StackedGraphPlaceholder/StackedGraphPlaceholder.tsx
@@ -2,16 +2,33 @@ import styles from './StackedGraphPlaceholder.module.scss'
 import { InfoCircleOutlined } from '@ant-design/icons'
 import { useTranslation } from 'react-i18next'
 import { StackedGraphPlaceholderProps } from 'modules/graph/components/StackedGraphPlaceholder/types'
+import { useSleepContext } from 'context'
+import { useMemo } from 'react'
+import { SleepMetric } from 'modules/controls/MetricConfiguration'
+import { MetricCheckbox } from 'modules/controls/MetricCheckbox'
 
 export const StackedGraphPlaceholder = ({ id }: StackedGraphPlaceholderProps) => {
-  const { t } = useTranslation('translation', { keyPrefix: 'sleep.graph2d' })
-  
+  const { stackedMetrics } = useSleepContext()
+  const { t } = useTranslation('translation', { keyPrefix: 'sleep.graph2d.placeholder' })
+
+  const availableMetrics = useMemo<SleepMetric[]>(() => {
+    const allMetrics = Object.values(SleepMetric)
+    return allMetrics.filter(metric => !stackedMetrics.includes(metric))
+  }, [stackedMetrics])
+
   return (
     <div className={styles.placeholder} key={`graph-placeholder-${id}`}>
-      <InfoCircleOutlined className={styles.infoIcon}/>
+      <InfoCircleOutlined className={styles.infoIcon} />
+
       <p className={styles.selectText}>
-        {t('select-second-metric')}
+        {t(`message.${id === 0 ? 'first' : 'second'}`)}
       </p>
+
+      <div>
+        {availableMetrics.map(metric => (
+          <MetricCheckbox label={metric} metric={metric} />
+        ))}
+      </div>
     </div>
   )
 }
