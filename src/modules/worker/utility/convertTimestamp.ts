dad2e466697a68f0280c3937cf64bfb7fc26ca01
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2024-11-18 17:06:00 +0000

    chore(data): updated docs regarding Apples Cocoa Datetime API

--- src/modules/worker/utility/convertTimestamp.ts
+++ src/modules/worker/utility/convertTimestamp.ts
@@ -1,8 +1,8 @@
 /**
- * For some reason the raw Pillow database export
- * has fractional UNIX timestamps that have correct
- * dates and months but the years are 31 years in
- * the past.
+ * Since Pillow is an iOS application, it uses the Cocoa Datetime API
+ * from the Core Data storage framework. Timestamps from this API
+ * are stored as seconds from January 1st 2001, unlike UNIX timestamps
+ * which used milliseconds from January 1st 1970 00:00 (Epoch).
  *
  * For example, the timestamp might be 557367577.554087,
  * which is Mon Aug 31 1987 01:19:37 GMT+0100 (British Summer Time).
@@ -11,7 +11,7 @@
  * @param rawTimestamp The raw timestamp from the export.
  */
 export const convertTimestamp = (rawTimestamp: number): Date => {
-  const date = new Date(rawTimestamp * 1000)
-  date.setFullYear(date.getFullYear() + 31)
+  const date = new Date(rawTimestamp * 1000) // <-- Convert seconds -> milliseconds
+  date.setFullYear(date.getFullYear() + 31) // <-- (2001 - 1970) = 31 years, so add 31 years
   return date
 }
