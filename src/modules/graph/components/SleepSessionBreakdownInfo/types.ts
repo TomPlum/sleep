acd7649ba46f326cc5cc8f794d4dea4e3604f5b5
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2024-11-23 19:29:30 +0000

    chore(graph): extracted SleepSessionBreakdownInfo.tsx component

---
+++ src/modules/graph/components/SleepSessionBreakdownInfo/types.ts
@@ -0,0 +1,5 @@
+import { SleepSessionGraph2DDatum } from 'modules/graph/components/SleepSessionsGraph2D'
+
+export interface SleepSessionBreakdownInfoProps {
+  session: SleepSessionGraph2DDatum
+}
--- src/modules/graph/components/SleepSessionInfo/SleepSessionInfo.tsx
+++ src/modules/graph/components/SleepSessionInfo/SleepSessionInfo.tsx
@@ -1,17 +1,13 @@
 import { SleepSessionStageBreakdownGraph } from 'modules/graph/components/SleepSessionStageBreakdownGraph'
 import styles from './SleepSessionInfo.module.scss'
-import dayjs from 'dayjs'
 import { useSleepContext } from 'context'
-import { DurationBreakdownPie, DurationBreakdownPieData } from 'modules/graph/components/DurationBreakdownPie'
-import { useEffect, useMemo, useState } from 'react'
-import { SleepMetric } from 'modules/controls/MetricConfiguration'
+import { useEffect, useState } from 'react'
 import { useQueryParams } from 'hooks/useQueryParams'
 import { SleepSessionGraph2DDatum } from 'modules/graph/components/SleepSessionsGraph2D'
-import { useTranslation } from 'react-i18next'
+import { SleepSessionBreakdownInfo } from 'modules/graph/components/SleepSessionBreakdownInfo'
 
 export const SleepSessionInfo = () => {
   const { queryParams } = useQueryParams()
-  const { t } = useTranslation('translation', { keyPrefix: 'sleep.graph2d.sleep-session-info' })
   const { graphData2d, sleepStageData, sleepSoundData, selectedSession: id } = useSleepContext()
 
   const [selectedSession, setSelectedSession] = useState<SleepSessionGraph2DDatum>()
@@ -27,27 +23,9 @@ export const SleepSessionInfo = () => {
     }
   }, [id, graphData2d.data, queryParams.selected])
 
-  const pieData = useMemo<DurationBreakdownPieData | undefined>(() => {
-    if (!selectedSession) {
-      return undefined
-    }
-
-    return ({
-      awake: selectedSession[SleepMetric.AWAKE_TIME],
-      deep: selectedSession[SleepMetric.DEEP_SLEEP],
-      light: selectedSession[SleepMetric.LIGHT_SLEEP],
-      rem: selectedSession[SleepMetric.REM_SLEEP]
-    })
-  }, [selectedSession])
-
   if (!selectedSession || !sleepStageData || !sleepSoundData) {
     return null
   }
-
-  const startTime = dayjs(selectedSession.date)
-  const endTime = dayjs(selectedSession.endTime).add(1, 'hour') // TODO: Check this +1 hour. Its offset GMT+1
-  const hoursDifference = endTime.diff(startTime, 'hours')
-  const remainingMinutes = endTime.diff(startTime, 'minutes') % hoursDifference
 
   return (
     <div className={styles.container}>
@@ -56,25 +34,7 @@ export const SleepSessionInfo = () => {
         sounds={sleepSoundData[selectedSession.id]}
       />
 
-      <div className={styles.info}>
-        <p className={styles.text}>
-          {startTime.format('ddd Do MMM YYYY')}
-        </p>
-
-        <p className={styles.text}>
-          {startTime.format('HH:mm')}
-          {' -> '}
-          {endTime.format('HH:mm')}
-          {' '}
-          ({hoursDifference}{t('hour')} {remainingMinutes}{t('minute')})
-        </p>
-
-        {pieData && (
-          <div className={styles.pieContainer}>
-            <DurationBreakdownPie data={pieData}/>
-          </div>
-        )}
-      </div>
+     <SleepSessionBreakdownInfo session={selectedSession} />
     </div>
   )
 }
