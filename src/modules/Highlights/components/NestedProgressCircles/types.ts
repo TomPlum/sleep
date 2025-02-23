f7cd757b4a8d12cf7451fcb4d0698fb34fdf688b
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2025-02-23 18:48:35 +0000

    chore(docs): added tsdoc to NestedProgressCircle props interface

--- src/modules/Highlights/components/NestedProgressCircles/types.ts
+++ src/modules/Highlights/components/NestedProgressCircles/types.ts
@@ -1,9 +1,51 @@
 export interface NestedProgressCirclesProps {
-  size: number;
+  /**
+   * The diameter of the outer
+   * progress circle in pixels.
+   */
+  size: number
+
+  /**
+   * The width, in pixels, of the progress
+   * stroke that moves along the circular track.
+   */
   strokeWidth: number
+
+  /**
+   * The percentage value of the outer circle.
+   * This value determines how much of the track
+   * is filled with the progress stroke.
+   *
+   * 100% will be a complete circle, where-as 0%
+   * will have no progress and just an empty track.
+   */
   outerPercent: number
+
+  /**
+   * The colour of the outer circles'
+   * progress stroke.
+   */
   outerColor: string
+
+  /**
+   * The percentage value of the inner circle.
+   * This value determines how much of the track
+   * is filled with the progress stroke.
+   *
+   * 100% will be a complete circle, where-as 0%
+   * will have no progress and just an empty track.
+   */
   innerPercent: number
+
+  /**
+   * The colour of the inner circles'
+   * progress stroke.
+   */
   innerColor: string
+
+  /**
+   * An optional classname to pass to
+   * the container around the circles.
+   */
   className?: string
 }
