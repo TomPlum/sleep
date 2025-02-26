0c202afe2d2c6c074f7e81dd363bb1a1375a90c3
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2025-02-26 17:59:21 +0000

    feat(styling): making scene more responsive and full screen

--- src/pages/HighlightsPage.tsx
+++ src/pages/HighlightsPage.tsx
@@ -5,11 +5,12 @@ import { useHighlightedSessions } from 'modules/Highlights/hooks/useHighlightedS
 import { Button, Carousel, Typography } from 'antd'
 import { BestSessionShowcase } from 'modules/Highlights/components/BestSessionShowcase'
 import { OverviewShowcase } from 'modules/Highlights/components/OverviewShowcase/OverviewShowcase'
-import { useState } from 'react'
+import { CSSProperties, useMemo, useState } from 'react'
 import { PillowDataFileLink } from 'components/PillowDataFileLink'
 import { useTranslation } from 'react-i18next'
 import { SleepingAnimation } from 'modules/Highlights/components/SleepingAnimation'
 import classNames from 'classnames'
+import { useWindowSize } from '@uidotdev/usehooks'
 
 // Overview, total session, time slept, percentage of time slept compared to days etc
 // Best Session
@@ -17,12 +18,38 @@ import classNames from 'classnames'
 // Most Recent Session
 // Trends over last N months
 
+const getRandomInt = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
 export const HighlightsPage = () => {
+  const { width } = useWindowSize()
   const { isSleepDataLoading } = useSleepContext()
   const { t } = useTranslation('translation', { keyPrefix: 'highlights.landing' })
   const { bestSession, worstSession, mostRecentSession } = useHighlightedSessions()
 
   const [started, setStarted] = useState(false)
+
+  const path = useMemo(() => {
+    const step = width === null || width < 1000 ? 4 : 2
+    const quantity = width == null ? 75 : width > 1000 ? 100 : 50
+
+    const values = Array(quantity).fill('').map((_v, i) => {
+      const depth = getRandomInt(60, 150)
+      return `${(i + 1) * step}% ${depth}px`
+    })
+
+    values.unshift('0% 0%') // <-- Top left [2]
+    values.unshift('0% 100%') // <-- Bottom left [1]
+    // Values from above are in the middle here
+    values.push('100% 0%') // <-- Top Right [3]
+    values.push('100% 100%') // <-- Bottom Right [4]
+
+    return {
+      '--path': values.join(',')
+    }
+  }, [width]) as CSSProperties
+  console.log(path)
   
   if (isSleepDataLoading) {
     return (
@@ -48,29 +75,26 @@ export const HighlightsPage = () => {
         <div className={classNames(styles.star, styles.s5)}></div>
         <div className={classNames(styles.star, styles.s6)}></div>
         <div className={classNames(styles.star, styles.s7)}></div>
+
         <div className={styles.moon1}></div>
         <div className={styles.moon}></div>
         <div className={styles.shooting}></div>
-        <div className={styles.mountains1}>
-          <div className={styles.m1}></div>
-          <div className={styles.m2}></div>
-          <div className={styles.m3}></div>
-          <div className={styles.m4}></div>
-        </div>
-        <div className={styles.land}>
-          <div className={styles.windmill}>
-            <div className={styles.light}></div>
-            <div className={styles.door}></div>
-            <div className={styles.top}></div>
-            <div className={styles.blades}>
-              <div className={classNames(styles.bl, styles.bl1)}></div>
-              <div className={classNames(styles.bl, styles.bl2)}></div>
-              <div className={classNames(styles.bl, styles.bl3)}></div>
-              <div className={classNames(styles.bl, styles.bl4)}></div>
-            </div>
+
+        <div className={styles.mountains} style={path} />
+
+        <div className={styles.land} />
+        <div className={styles.windmill}>
+          <div className={styles.light}></div>
+          <div className={styles.door}></div>
+          <div className={styles.top}></div>
+          <div className={styles.blades}>
+            <div className={classNames(styles.bl, styles.bl1)}></div>
+            <div className={classNames(styles.bl, styles.bl2)}></div>
+            <div className={classNames(styles.bl, styles.bl3)}></div>
+            <div className={classNames(styles.bl, styles.bl4)}></div>
           </div>
-          <div className={styles.tree}></div>
         </div>
+        <div className={styles.tree}></div>
       </div>
 
       <div className={styles.content}>
