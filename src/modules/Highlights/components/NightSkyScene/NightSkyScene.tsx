a267aea42b789d9fb70c1d833a73983cf1528eeb
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2025-02-26 19:42:48 +0000

    feat(styling): extracted night sky scene into component

---
+++ src/modules/Highlights/components/NightSkyScene/NightSkyScene.tsx
@@ -0,0 +1,114 @@
+import styles from './NightSkyScene.module.scss'
+import classNames from 'classnames'
+import { CSSProperties, useMemo } from 'react'
+import { useWindowSize } from '@uidotdev/usehooks'
+
+const getRandomInt = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+const generateForegroundStars = () => {
+  return Array(18).fill(0).map((_, i) => {
+    // Between 0.1 and 1
+    const animationDelay = Math.random() * 0.9 + 0.1
+
+    // Between 10 and 50
+    const top = Math.random() * (50 - 10) + 10
+
+    // Between 15 and 80
+    const left = Math.random() * (80 - 15) + 15
+
+    return (
+      <div
+        key={i}
+        className={styles.foregroundStar}
+        style={{
+          top: `${top}%`,
+          left: `${left}%`,
+          animationDelay: `${animationDelay}s`,
+          WebkitAnimationDelay: `${animationDelay}s`,
+        }}
+      />
+    )
+  })
+}
+
+const generateBackgroundStars = () => {
+  return Array(50).fill(0).map((_, i) => {
+    // Between 5 and 55
+    const top = Math.random() * (55 - 10) + 5
+
+    // Between 5 and 95
+    const left = Math.random() * (95 - 15) + 5
+
+    return (
+      <div
+        key={i}
+        className={styles.backgroundStar}
+        style={{
+          top: `${top}%`,
+          left: `${left}%`
+        }}
+      />
+    )
+  })
+}
+
+export const NightSkyScene = () => {
+  const { width } = useWindowSize()
+
+  const path = useMemo(() => {
+    const step = width === null || width < 1000 ? 4 : 2
+    const quantity = width == null ? 75 : width > 1000 ? 100 : 50
+
+    const values = Array(quantity).fill('').map((_v, i) => {
+      const depth = getRandomInt(60, 150)
+      return `${(i + 1) * step}% ${depth}px`
+    })
+
+    values.unshift('0% 0%') // <-- Top left [2]
+    values.unshift('0% 100%') // <-- Bottom left [1]
+    // Values from above are in the middle here
+    values.push('100% 0%') // <-- Top Right [3]
+    values.push('100% 100%') // <-- Bottom Right [4]
+
+    return {
+      '--path': values.join(',')
+    }
+  }, [width]) as CSSProperties
+
+  return (
+    <div className={styles.scene}>
+      <div className={styles.foregroundStars}>
+        {generateForegroundStars()}
+      </div>
+
+      <div className={styles.backgroundStars}>
+        {generateBackgroundStars()}
+      </div>
+
+      <div className={styles.moon} />
+
+      <div className={styles.shootingStar1} />
+      <div className={styles.shootingStar2} />
+
+      <div className={styles.mountains} style={path} />
+
+      <div className={styles.land} />
+
+      <div className={styles.windmill}>
+        <div className={styles.light} />
+        <div className={styles.door} />
+        <div className={styles.top} />
+        <div className={styles.blades}>
+          <div className={classNames(styles.bl, styles.blade1)} />
+          <div className={classNames(styles.bl, styles.blade2)} />
+          <div className={classNames(styles.bl, styles.blade3)} />
+          <div className={classNames(styles.bl, styles.blade4)} />
+        </div>
+      </div>
+
+      <div className={styles.tree} />
+    </div>
+  )
+}
--- src/pages/HighlightsPage.tsx
+++ src/pages/HighlightsPage.tsx
@@ -5,97 +5,23 @@ import { useHighlightedSessions } from 'modules/Highlights/hooks/useHighlightedS
 import { Button, Carousel, Typography } from 'antd'
 import { BestSessionShowcase } from 'modules/Highlights/components/BestSessionShowcase'
 import { OverviewShowcase } from 'modules/Highlights/components/OverviewShowcase/OverviewShowcase'
-import { CSSProperties, useMemo, useState } from 'react'
+import { useState } from 'react'
 import { PillowDataFileLink } from 'components/PillowDataFileLink'
 import { useTranslation } from 'react-i18next'
 import { SleepingAnimation } from 'modules/Highlights/components/SleepingAnimation'
-import classNames from 'classnames'
-import { useWindowSize } from '@uidotdev/usehooks'
+import { NightSkyScene } from 'modules/Highlights/components/NightSkyScene'
 
 // Overview, total session, time slept, percentage of time slept compared to days etc
 // Best Session
 // Worst Session
 // Most Recent Session
 // Trends over last N months
-
-const getRandomInt = (min: number, max: number) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
-
 export const HighlightsPage = () => {
-  const { width } = useWindowSize()
   const { isSleepDataLoading } = useSleepContext()
   const { t } = useTranslation('translation', { keyPrefix: 'highlights.landing' })
   const { bestSession, worstSession, mostRecentSession } = useHighlightedSessions()
 
   const [started, setStarted] = useState(false)
-
-  const path = useMemo(() => {
-    const step = width === null || width < 1000 ? 4 : 2
-    const quantity = width == null ? 75 : width > 1000 ? 100 : 50
-
-    const values = Array(quantity).fill('').map((_v, i) => {
-      const depth = getRandomInt(60, 150)
-      return `${(i + 1) * step}% ${depth}px`
-    })
-
-    values.unshift('0% 0%') // <-- Top left [2]
-    values.unshift('0% 100%') // <-- Bottom left [1]
-    // Values from above are in the middle here
-    values.push('100% 0%') // <-- Top Right [3]
-    values.push('100% 100%') // <-- Bottom Right [4]
-
-    return {
-      '--path': values.join(',')
-    }
-  }, [width]) as CSSProperties
-
-  const generateForegroundStars = () => {
-    return Array(18).fill(0).map((_, i) => {
-      // Between 0.1 and 1
-      const animationDelay = Math.random() * 0.9 + 0.1
-
-      // Between 10 and 50
-      const top = Math.random() * (50 - 10) + 10
-
-      // Between 15 and 80
-      const left = Math.random() * (80 - 15) + 15
-
-      return (
-        <div
-          key={i}
-          className={styles.foregroundStar}
-          style={{
-            top: `${top}%`,
-            left: `${left}%`,
-            animationDelay: `${animationDelay}s`,
-            WebkitAnimationDelay: `${animationDelay}s`,
-          }}
-        />
-      )
-    })
-  }
-
-  const generateBackgroundStars = () => {
-    return Array(50).fill(0).map((_, i) => {
-      // Between 5 and 55
-      const top = Math.random() * (55 - 10) + 5
-
-      // Between 5 and 95
-      const left = Math.random() * (95 - 15) + 5
-
-      return (
-        <div
-          key={i}
-          className={styles.backgroundStar}
-          style={{
-            top: `${top}%`,
-            left: `${left}%`
-          }}
-        />
-      )
-    })
-  }
 
   if (isSleepDataLoading) {
     return (
@@ -113,38 +39,7 @@ export const HighlightsPage = () => {
 
   return (
     <div className={styles.page}>
-      <div className={styles.scene}>
-        <div className={styles.foregroundStars}>
-          {generateForegroundStars()}
-        </div>
-
-        <div className={styles.backgroundStars}>
-          {generateBackgroundStars()}
-        </div>
-
-        <div className={styles.moon} />
-
-        <div className={styles.shootingStar1} />
-        <div className={styles.shootingStar2} />
-
-        <div className={styles.mountains} style={path} />
-
-        <div className={styles.land} />
-
-        <div className={styles.windmill}>
-          <div className={styles.light} />
-          <div className={styles.door} />
-          <div className={styles.top} />
-          <div className={styles.blades}>
-            <div className={classNames(styles.bl, styles.blade1)} />
-            <div className={classNames(styles.bl, styles.blade2)} />
-            <div className={classNames(styles.bl, styles.blade3)} />
-            <div className={classNames(styles.bl, styles.blade4)} />
-          </div>
-        </div>
-
-        <div className={styles.tree} />
-      </div>
+      <NightSkyScene />
 
       <div className={styles.content}>
         <div className={styles.header}>
