cf56b70a804d123a4aa8691985cae52370369524
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2025-02-26 17:27:45 +0000

    feat(styling): experimenting with highlights landing styling

--- src/pages/HighlightsPage.tsx
+++ src/pages/HighlightsPage.tsx
@@ -9,6 +9,7 @@ import { useState } from 'react'
 import { PillowDataFileLink } from 'components/PillowDataFileLink'
 import { useTranslation } from 'react-i18next'
 import { SleepingAnimation } from 'modules/Highlights/components/SleepingAnimation'
+import classNames from 'classnames'
 
 // Overview, total session, time slept, percentage of time slept compared to days etc
 // Best Session
@@ -39,6 +40,39 @@ export const HighlightsPage = () => {
 
   return (
     <div className={styles.page}>
+      <div className={styles.scene}>
+        <div className={classNames(styles.star, styles.s1)}></div>
+        <div className={classNames(styles.star, styles.s2)}></div>
+        <div className={classNames(styles.star, styles.s3)}></div>
+        <div className={classNames(styles.star, styles.s4)}></div>
+        <div className={classNames(styles.star, styles.s5)}></div>
+        <div className={classNames(styles.star, styles.s6)}></div>
+        <div className={classNames(styles.star, styles.s7)}></div>
+        <div className={styles.moon1}></div>
+        <div className={styles.moon}></div>
+        <div className={styles.shooting}></div>
+        <div className={styles.mountains1}>
+          <div className={styles.m1}></div>
+          <div className={styles.m2}></div>
+          <div className={styles.m3}></div>
+          <div className={styles.m4}></div>
+        </div>
+        <div className={styles.land}>
+          <div className={styles.windmill}>
+            <div className={styles.light}></div>
+            <div className={styles.door}></div>
+            <div className={styles.top}></div>
+            <div className={styles.blades}>
+              <div className={classNames(styles.bl, styles.bl1)}></div>
+              <div className={classNames(styles.bl, styles.bl2)}></div>
+              <div className={classNames(styles.bl, styles.bl3)}></div>
+              <div className={classNames(styles.bl, styles.bl4)}></div>
+            </div>
+          </div>
+          <div className={styles.tree}></div>
+        </div>
+      </div>
+
       <div className={styles.content}>
         <div className={styles.header}>
           <Typography className={styles.heading}>
